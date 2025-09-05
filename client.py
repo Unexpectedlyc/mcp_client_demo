f8c7d91018991f9a130fb2051a94f430e5df83cb
yanchang <315824524@qq.com>
2025-09-05 13:59:41 +0800

    TODO:调用魔搭api时会报错工具josn 格式错误，需要进一步排查

--- client.py
+++ client.py
@@ -63,36 +63,37 @@ class MCPClient:
         # Process response and handle tool calls
         final_text = []
 
-        for content in response.content:
-            if content.type == 'text':
-                final_text.append(content.text)
-            elif content.type == 'tool_use':
-                tool_name = content.name
-                tool_args = content.input
+        # TODO 调用魔搭api时会报错工具josn 格式错误，需要进一步排查
+        # for content in response.content:
+        #     if content.type == 'text':
+        #         final_text.append(content.text)
+        #     elif content.type == 'tool_use':
+        #         tool_name = content.name
+        #         tool_args = content.input
                 
-                # Execute tool call
-                result = await self.session.call_tool(tool_name, tool_args)
-                final_text.append(f"[Calling tool {tool_name} with args {tool_args}]")
+        #         # Execute tool call
+        #         result = await self.session.call_tool(tool_name, tool_args)
+        #         final_text.append(f"[Calling tool {tool_name} with args {tool_args}]")
 
-                # Continue conversation with tool results
-                if hasattr(content, 'text') and content.text:
-                    messages.append({
-                      "role": "assistant",
-                      "content": content.text
-                    })
-                messages.append({
-                    "role": "user", 
-                    "content": result.content
-                })
+        #         # Continue conversation with tool results
+        #         if hasattr(content, 'text') and content.text:
+        #             messages.append({
+        #               "role": "assistant",
+        #               "content": content.text
+        #             })
+        #         messages.append({
+        #             "role": "user", 
+        #             "content": result.content
+        #         })
 
-                # Get next response from Claude
-                response = self.anthropic.messages.create(
-                    model="claude-3-5-sonnet-20241022",
-                    max_tokens=1000,
-                    messages=messages,
-                )
+        #         # Get next response from Claude
+        #         response = self.anthropic.messages.create(
+        #             model="claude-3-5-sonnet-20241022",
+        #             max_tokens=1000,
+        #             messages=messages,
+        #         )
 
-                final_text.append(response.content[0].text)
+        #         final_text.append(response.content[0].text)
 
         return "\n".join(final_text)
 
